4e9337697c828e106120652df3215f7b25a88b54
pedroizidoro <pedroizidoro@tce.al.gov.br>
2020-01-17 14:16:09 -0300

    Integrando com redux

--- src/dashboard/dashboardActions.js
+++ src/dashboard/dashboardActions.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
+const BASE_URL = 'https://my-json-server.typicode.com/pedroxavier64/json/summary'
 
-export default function getSummary() {
-
+export function getSummary() {
+    const request = axios.get(`${BASE_URL}`)
+    return {
+        type: 'BILLING_SUMMARY_FETCHED',
+        payload: request
+    }
 }
