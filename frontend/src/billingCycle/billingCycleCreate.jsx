3e4cea91f127c1e6e839962252902bf187f54bfe
pedroizidoro <pedroizidoro@tce.al.gov.br>
2020-02-04 09:53:41 -1200

    Finalizando app Ciclo de pagamentos

--- frontend/src/billingCycle/billingCycleCreate.jsx
+++ frontend/src/billingCycle/billingCycleCreate.jsx
@@ -12,19 +12,21 @@ class BillingCycleForm extends Component{
     calculateSummary() {
         const sum = (t, v) => t + v
         return {
-            sumOfCredits: this.props.credits.map()
+            sumOfCredits: this.props.credits.map(c => +c.value || 0 ).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0 ).reduce(sum)
         }
     }
 
     render() {
         const { handleSubmit, readOnly , credits, debts} = this.props
+        const { sumOfCredits, sumOfDebts} = this.calculateSummary()
         return (
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
                     <Field name="name" component={Input} readOnly={readOnly} cols="12 4" label="Nome:" placeholder="Nome do ciclo" type="text"/>
                     <Field name="month" component={Input} readOnly={readOnly} cols="12 4" label="Mês:" placeholder="Mês do ciclo" type="number"/>
                     <Field name="year" component={Input} readOnly={readOnly} cols="12 4" label="Ano:" placeholder="Ano do ciclo" type="number"/>
-                    <Summary/>
+                    <Summary credit={sumOfCredits} debts={sumOfDebts}/>
                     <ItemList cols='12 6' list={credits} readOnly={readOnly} field="credits" legend="Créditos"/>
                     <ItemList cols='12 6' list={debts} readOnly={readOnly} field="debts" legend="Débitos" showStatus={true}/>
                 </div>
