d790814b4e1fdf45a2662e8912b26663df040847
Pedro Henrique Xavier Izidoro Lima <pedroizidoro@tce.net>
2020-02-09 11:47:46 -1200

    Revertendo push merge

--- frontend-old/src/common/template/footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default props => (
-    <footer className="main-footer">
-        <strong>Tribunal de Contas do Estado de Alagoas - TCEAL 2019 |</strong>
-        <a href="https://www.tceal.tc.br/" target="_blank"> www.tceal.tc.br</a>.
-    </footer>
-)
